f52063f4764d8d0d11f41449198db9b43f3c2768
Pax <pax.margret@gmail.com>
2019-07-20 11:47:05 -0700

    Add pepperplate form

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import SEO from "../components/seo"
 import Step from "../components/step"
 
 import dropbox from "../images/dropbox.svg"
+import styles from "../css/form.module.css"
 
 const IndexPage = () => (
   <Layout>
@@ -11,9 +12,26 @@ const IndexPage = () => (
     <Step>
       <a href="/" className="uppercase font-bold">
         <h2>
-        Connect to <img alt="Dropbox" src={dropbox} className="h-8" />
+          Connect to <img alt="Dropbox" src={dropbox} className="h-8" />
         </h2>
       </a>
+    </Step>
+    <Step>
+      <h2 className="uppercase font-bold mb-4">
+        Enter your Pepperplate credentials
+      </h2>
+      <form className={styles.form}>
+        <label className="text-gray-700">Username</label>
+        <input
+          type="text"
+          className="shadow border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-inner"
+        />
+        <label className="text-gray-700">Password</label>
+        <input
+          type="password"
+          className="shadow border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-inner"
+        />
+      </form>
     </Step>
   </Layout>
 )
