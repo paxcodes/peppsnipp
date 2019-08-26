8278f4755e0c9577fb7149e0c19835908bbc1119
Pax <pax.margret@gmail.com>
2019-08-25 19:40:50 -0700

    Implement sessions

--- application/dropbox/__init__.py
+++ application/dropbox/__init__.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session
 
 dropbox_blueprint = Blueprint('dropbox_blueprint', __name__)
  
 @dropbox_blueprint.route('/dropbox', methods=['GET'])
 def hello_dropbox():
+  session['message'] = 'Hello World!'
   return jsonify({
       "msg": "Hello Dropbox!"
   })
--- tests/test_application.py
+++ tests/test_application.py
@@ -1,5 +1,12 @@
 from application import create_app
+from flask import session
 
 def test_appLoadsConfig():
   app = create_app()
   assert app.config['FLASK_APP'] == "Pepperplate Snipper"
+
+def test_session():
+  app = create_app()
+  with app.test_client() as client:
+    client.get('/dropbox')
+    assert session['message'] == 'Hello World!'
