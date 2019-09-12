cbd7712f794ce017b0af8d9b0fbbe830ef8566f7
Pax <pax.margret@gmail.com>
2019-09-11 23:10:07 -0700

    Open DropboxOAuthFlow in new window

--- cypress/integration/dropbox.test.js
+++ cypress/integration/dropbox.test.js
@@ -1,10 +1,21 @@
-describe("When I click on Dropbox", () => {
-  it('it links to the dropbox/start API endpoint', () => {
+describe("The 'Dropbox' step", () => {
+  it('links to the dropbox/start API endpoint', () => {
     cy.visit("/");
     cy.get('[data-cy=dropbox-oauth]')
       .invoke('attr', 'href')
       .then(href => {
         expect(href).to.match(/https\:\/\/.+\/dropbox\/start$/);
       });
-  })
-})
+  });
+  
+  it('opens a new window when clicked', () => {
+    cy.visit("/", {
+      onBeforeLoad(win) {
+        cy.stub(win, 'open');
+      }
+    });
+    
+    cy.get('[data-cy=dropbox-oauth]').click();
+    cy.window().its('open').should('be.called');
+  });
+});
