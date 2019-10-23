47910a4140151d9f6a708cdcb27712a0d3a08e0d
Pax <pax.margret@gmail.com>
2019-10-23 15:56:04 -0700

    Convert react component to a class

--- src/components/dropbox-step.js
+++ src/components/dropbox-step.js
@@ -1,29 +1,31 @@
 import React from "react"
 import dropbox from "../images/dropbox.svg"
 
-const DropboxStep = () => {
-  let popup;
-  function openNewWindow(e) {
+class DropboxStep extends React.Component {
+  updateDropboxStep = e => {
+    this.setState({dropboxConnected: e.data.success})
+    this.popup.close()
+  }
+  
+  openNewWindow = e => {
     e.preventDefault();
-    popup = window.top.open(e.currentTarget.attributes.href.value, "DropboxOAuth", "width=670, height=825, location=0");
-    window.addEventListener('message', updateDropboxStep)
+    this.popup = window.top.open(e.currentTarget.attributes.href.value, "DropboxOAuth", "width=670, height=825, location=0");
+    window.addEventListener('message', this.updateDropboxStep)
   }
   
-  function updateDropboxStep(e) {
-    
-    popup.close()
-  }
-  
-  return (
+  render() {
+    return (
     <>
-      <a href={`${process.env.API_URL}/dropbox/start`} data-cy="dropbox-oauth" className="block" onClick={openNewWindow}>
+      <a href={`${process.env.API_URL}/dropbox/start`} data-cy="dropbox-oauth" className="block" onClick={this.openNewWindow}>
         <h2 className="uppercase font-bold mb-4">
           Connect to <img alt="Dropbox" src={dropbox} className="h-8 mx-auto" />
         </h2>
       </a>
       <p>We will save screenshots of your recipes in your Dropbox account.</p>
     </>
-  )
+    )
+  }
 }
+
 
 export default DropboxStep
