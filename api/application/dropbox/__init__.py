e088322bfa87083d30095d945e23f4c2a419a456
Pax <pax.margret@gmail.com>
2019-10-30 14:10:16 -0700

    Follow Python formatting

--- api/application/dropbox/__init__.py
+++ api/application/dropbox/__init__.py
@@ -4,38 +4,40 @@ from application.api_error import APIError
 from dropbox.oauth import *
 
 dropbox_blueprint = Blueprint('dropbox_blueprint', __name__)
- 
+
+
 @dropbox_blueprint.route('/dropbox', methods=['GET'])
 def hello_dropbox():
-  session['message'] = 'Hello World!'
-  return jsonify({
-      "msg": "Hello Dropbox!"
-  })
+    session['message'] = 'Hello World!'
+    return jsonify({
+        "msg": "Hello Dropbox!"
+    })
+
 
 @dropbox_blueprint.route('/dropbox/start')
 def dropbox_oauth_start():
-  authorize_url = get_dropbox_auth_flow().start()
-  return redirect(authorize_url)
+    authorize_url = get_dropbox_auth_flow().start()
+    return redirect(authorize_url)
 
 
 @dropbox_blueprint.route('/dropbox/finish')
 def dropbox_oauth_finish():
-  try:
-    oauth_result = get_dropbox_auth_flow().finish(request.args)
-    session['dropbox-access-token'] = oauth_result.access_token
-    return jsonify({
+    try:
+        oauth_result = get_dropbox_auth_flow().finish(request.args)
+        session['dropbox-access-token'] = oauth_result.access_token
+        return jsonify({
       "success": True
-    })
-  except BadRequestException as e:
-    error_message = e.args[0]
-    raise APIError(error_message)
-  except BadStateException:
-    return redirect("/dropbox/start")
+        })
+    except BadRequestException as e:
+        error_message = e.args[0]
+        raise APIError(error_message)
+    except BadStateException:
+        return redirect("/dropbox/start")
+
 
 def get_dropbox_auth_flow():
-  DROPBOX_APP_KEY = app.config['DROPBOX_APP_KEY']
-  DROPBOX_APP_SECRET = app.config['DROPBOX_APP_SECRET']
-  REDIRECT_URI = app.config['URL'] + "/dropbox/finish"
-  return DropboxOAuth2Flow(DROPBOX_APP_KEY, DROPBOX_APP_SECRET, 
-    REDIRECT_URI, session, "dropbox-auth-csrf-token")
-  
+    DROPBOX_APP_KEY = app.config['DROPBOX_APP_KEY']
+    DROPBOX_APP_SECRET = app.config['DROPBOX_APP_SECRET']
+    REDIRECT_URI = app.config['URL'] + "/dropbox/finish"
+    return DropboxOAuth2Flow(DROPBOX_APP_KEY, DROPBOX_APP_SECRET,
+                             REDIRECT_URI, session, "dropbox-auth-csrf-token")
