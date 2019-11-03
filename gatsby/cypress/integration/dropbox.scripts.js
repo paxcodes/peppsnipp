3659f5f0cc9b561ffc6b59d3730ed185d1e7a7c9
Pax <pax.margret@gmail.com>
2019-11-03 15:48:43 -0800

    Move actual scripts to a separate scripts file

---
+++ gatsby/cypress/integration/dropbox.scripts.js
@@ -0,0 +1,149 @@
+export const stubOpenAuthWindow = win => {
+   win.top.open = cy
+      .stub()
+      .as("openAuthWindow")
+      .callsFake(function(url, target, features, replace) {
+         return {
+            close: function() {}
+         };
+      });
+};
+
+export default {
+   linkShouldBeDropboxStart: () => {
+      cy.visit("/");
+      cy.get("[data-cy=dropbox-oauth]")
+         .invoke("attr", "href")
+         .then(href => {
+            expect(href).to.match(/https\:\/\/.+\/dropbox\/start$/);
+         });
+   },
+   clickingLinkShouldOpenNewWindow: () => {
+      cy.visit("/");
+      cy.get("[data-cy=dropbox-oauth]").click();
+
+      cy.window()
+         .its("top.open")
+         .should("be.called");
+   },
+   appShouldPrintErrorMessage: () => {
+      cy.visit("/dropbox-finish");
+      cy.getTestElement("errorText").should("be.visible");
+   },
+   appShouldCallTheAPI: () => {
+      const expectedParams = new URLSearchParams({
+         state: "sojhqf2nGunEIxI-MdePeg==",
+         code: "pQbO_7SMV2AAAAAAAAAAL_Tpd0uws8RpRTO2OBQAXwI"
+      });
+
+      cy.server();
+      cy.route(`https://localhost:5000/dropbox/finish?state=*&code=*`).as(
+         "finishOAuthFlow"
+      );
+
+      cy.visit("/dropbox-finish?" + expectedParams.toString(), {
+         onBeforeLoad(win) {
+            win.opener = win;
+         }
+      });
+
+      cy.wait("@finishOAuthFlow").then(xhr => {
+         const actualURL = new URL(xhr.url);
+         const actualParams = new URLSearchParams(actualURL.search);
+         expect(actualParams.get("state")).to.equal(
+            expectedParams.get("state")
+         );
+         expect(actualParams.get("code")).to.equal(expectedParams.get("code"));
+      });
+   },
+   popupShouldNotifyMainWindowOfSuccess: () => {
+      const expectedParams = new URLSearchParams({
+         state: "sojhqf2nGunEIxI-MdePeg==",
+         code: "pQbO_7SMV2AAAAAAAAAAL_Tpd0uws8RpRTO2OBQAXwI"
+      });
+
+      const stubbedResponse = { success: true };
+
+      cy.server();
+      cy.route(
+         `https://localhost:5000/dropbox/finish?state=*&code=*`,
+         stubbedResponse
+      ).as("finishOAuthFlow");
+
+      cy.visit("/dropbox-finish?" + expectedParams.toString(), {
+         onBeforeLoad(win) {
+            win.opener = win;
+            cy.stub(win.opener, "postMessage");
+         }
+      });
+
+      cy.window()
+         .its("opener.postMessage")
+         .should("be.calledWith", stubbedResponse);
+   },
+   popupShouldNotifyMainWindowOfFailure: () => {
+      const expectedParams = new URLSearchParams({
+         state: "sojhqf2nGunEIxI-MdePeg==",
+         code: "pQbO_7SMV2AAAAAAAAAAL_Tpd0uws8RpRTO2OBQAXwI"
+      });
+
+      const stubbedResponse = { success: false };
+
+      cy.server();
+      cy.route({
+         url: `https://localhost:5000/dropbox/finish?state=*&code=*`,
+         response: stubbedResponse,
+         status: 400
+      }).as("finishOAuthFlow");
+
+      cy.visit("/dropbox-finish?" + expectedParams.toString(), {
+         onBeforeLoad(win) {
+            win.opener = win;
+            cy.stub(win.opener, "postMessage");
+         }
+      });
+
+      cy.window()
+         .its("opener.postMessage")
+         .should("be.calledWith", stubbedResponse);
+   },
+   mainWindowShouldClosePopup: () => {
+      cy.visit("/");
+
+      cy.get("[data-cy=dropbox-oauth]").click();
+      cy.get("@openAuthWindow")
+         .should("be.called")
+         .then(function(spy) {
+            const popup = spy.returnValues[0];
+            cy.spy(popup, "close").as("closePopup");
+         });
+
+      cy.window().trigger("message", { data: { success: true } });
+      cy.get("@closePopup").should("be.called");
+   },
+   boxShouldPrintSuccessMessage: () => {
+      cy.visit("/");
+      cy.get("[data-cy=dropbox-oauth]").click();
+      cy.window().trigger("message", { data: { success: true } });
+
+      cy.get("[data-cy=dropbox-oauth]").should("not.be.visible");
+      cy.get("[data-cy=dropbox-oauth-success]").should("be.visible");
+   },
+   boxShouldPrintErrorMessage: () => {
+      cy.fixture("oauth400").then(oauth400Data => {
+         cy.visit("/");
+         cy.get("[data-cy=dropbox-oauth]").click();
+         cy.get("@openAuthWindow")
+            .should("be.called")
+            .then(function(mock) {
+               const popup = mock.returnValues[0];
+               cy.stub(popup, "close").as("closePopup");
+            });
+         cy.window().trigger("message", oauth400Data);
+      });
+
+      cy.get("[data-cy=dropbox-oauth]").should("be.visible");
+      cy.get("[data-cy=dropbox-oauth-success]").should("not.be.visible");
+      cy.get("[data-cy=dropbox-oauth-fail]").should("be.visible");
+   }
+};
--- gatsby/cypress/integration/dropbox.spec.js
+++ gatsby/cypress/integration/dropbox.spec.js
@@ -1,169 +1,46 @@
-Cypress.on("window:before:load", win => {
-   win.top.open = cy
-      .stub()
-      .as("openAuthWindow")
-      .callsFake(function(url, target, features, replace) {
-         return {
-            close: function() {}
-         };
-      });
-});
+import { stubOpenAuthWindow } from "./dropbox.scripts";
+import tests from "./dropbox.scripts";
+
+Cypress.on("window:before:load", win => stubOpenAuthWindow(win));
 
 describe("The 'Dropbox' step", () => {
-   it("links to the dropbox/start API endpoint", () => {
-      cy.visit("/");
-      cy.get("[data-cy=dropbox-oauth]")
-         .invoke("attr", "href")
-         .then(href => {
-            expect(href).to.match(/https\:\/\/.+\/dropbox\/start$/);
-         });
-   });
+   it(
+      "links to the dropbox/start API endpoint",
+      tests.linkShouldBeDropboxStart
+   );
+   it("opens a new window when clicked", tests.clickingLinkShouldOpenNewWindow);
+});
 
-   it("opens a new window when clicked", () => {
-      cy.visit("/");
-      cy.get("[data-cy=dropbox-oauth]").click();
-
-      cy.window()
-         .its("top.open")
-         .should("be.called");
-   });
-
-   context("When visiting the oAuth redirect link directly", () => {
-      specify("it will cause an error message to print", () => {
-         cy.visit("/dropbox-finish");
-         cy.getTestElement("errorText").should("be.visible");
-      });
-   });
+describe("When visiting the oAuth redirect link directly", () => {
+   it("will cause an error message to print", tests.appShouldPrintErrorMessage);
 });
 
 context("When Dropbox redirects back to our app", () => {
-   specify("the app calls the api to finish the oAuth process", () => {
-      const expectedParams = new URLSearchParams({
-         state: "sojhqf2nGunEIxI-MdePeg==",
-         code: "pQbO_7SMV2AAAAAAAAAAL_Tpd0uws8RpRTO2OBQAXwI"
-      });
-
-      cy.server();
-      cy.route(`https://localhost:5000/dropbox/finish?state=*&code=*`).as(
-         "finishOAuthFlow"
-      );
-
-      cy.visit("/dropbox-finish?" + expectedParams.toString(), {
-         onBeforeLoad(win) {
-            win.opener = win;
-         }
-      });
-
-      cy.wait("@finishOAuthFlow").then(xhr => {
-         const actualURL = new URL(xhr.url);
-         const actualParams = new URLSearchParams(actualURL.search);
-         expect(actualParams.get("state")).to.equal(
-            expectedParams.get("state")
-         );
-         expect(actualParams.get("code")).to.equal(expectedParams.get("code"));
-      });
-   });
+   specify(
+      "the app calls the api to finish the oAuth process",
+      tests.appShouldCallTheAPI
+   );
 });
 
 context("When the API finishes the oAuth process", () => {
    specify(
-      "the popup should notify parent window the successful response",
-      () => {
-         const expectedParams = new URLSearchParams({
-            state: "sojhqf2nGunEIxI-MdePeg==",
-            code: "pQbO_7SMV2AAAAAAAAAAL_Tpd0uws8RpRTO2OBQAXwI"
-         });
-
-         const stubbedResponse = { success: true };
-
-         cy.server();
-         cy.route(
-            `https://localhost:5000/dropbox/finish?state=*&code=*`,
-            stubbedResponse
-         ).as("finishOAuthFlow");
-
-         cy.visit("/dropbox-finish?" + expectedParams.toString(), {
-            onBeforeLoad(win) {
-               win.opener = win;
-               cy.stub(win.opener, "postMessage");
-            }
-         });
-
-         cy.window()
-            .its("opener.postMessage")
-            .should("be.calledWith", stubbedResponse);
-      }
+      "popup should notify main window of the successful response",
+      tests.popupShouldNotifyMainWindowOfSuccess
    );
-
-   specify("the popup should notify parent window the failed response", () => {
-      const expectedParams = new URLSearchParams({
-         state: "sojhqf2nGunEIxI-MdePeg==",
-         code: "pQbO_7SMV2AAAAAAAAAAL_Tpd0uws8RpRTO2OBQAXwI"
-      });
-
-      const stubbedResponse = { success: false };
-
-      cy.server();
-      cy.route({
-         url: `https://localhost:5000/dropbox/finish?state=*&code=*`,
-         response: stubbedResponse,
-         status: 400
-      }).as("finishOAuthFlow");
-
-      cy.visit("/dropbox-finish?" + expectedParams.toString(), {
-         onBeforeLoad(win) {
-            win.opener = win;
-            cy.stub(win.opener, "postMessage");
-         }
-      });
-
-      cy.window()
-         .its("opener.postMessage")
-         .should("be.calledWith", stubbedResponse);
-   });
-
-   specify("the parent window should close the popup", () => {
-      cy.visit("/");
-
-      cy.get("[data-cy=dropbox-oauth]").click();
-      cy.get("@openAuthWindow")
-         .should("be.called")
-         .then(function(spy) {
-            const popup = spy.returnValues[0];
-            cy.spy(popup, "close").as("closePopup");
-         });
-
-      cy.window().trigger("message", { data: { success: true } });
-      cy.get("@closePopup").should("be.called");
-   });
-
    specify(
-      "the Dropbox step should be updated (authentication successful)",
-      () => {
-         cy.visit("/");
-         cy.get("[data-cy=dropbox-oauth]").click();
-         cy.window().trigger("message", { data: { success: true } });
-
-         cy.get("[data-cy=dropbox-oauth]").should("not.be.visible");
-         cy.get("[data-cy=dropbox-oauth-success]").should("be.visible");
-      }
+      "the popup should notify main window the failed response",
+      tests.popupShouldNotifyMainWindowOfFailure
    );
-
-   specify("the Dropbox step should be updated (oauth fail)", () => {
-      cy.fixture("oauth400").then(oauth400Data => {
-         cy.visit("/");
-         cy.get("[data-cy=dropbox-oauth]").click();
-         cy.get("@openAuthWindow")
-            .should("be.called")
-            .then(function(mock) {
-               const popup = mock.returnValues[0];
-               cy.stub(popup, "close").as("closePopup");
-            });
-         cy.window().trigger("message", oauth400Data);
-      });
-
-      cy.get("[data-cy=dropbox-oauth]").should("be.visible");
-      cy.get("[data-cy=dropbox-oauth-success]").should("not.be.visible");
-      cy.get("[data-cy=dropbox-oauth-fail]").should("be.visible");
-   });
+   specify(
+      "the main window should close the popup",
+      tests.mainWindowShouldClosePopup
+   );
+   specify(
+      "the Dropbox step should print message saying authentication is successful",
+      tests.boxShouldPrintSuccessMessage
+   );
+   specify(
+      "the Dropbox step should be updated (oauth fail)",
+      tests.boxShouldPrintErrorMessage
+   );
 });
