6bd76bfb61a1633e886332c74a1db30cf5bcd47f
Pax <pax.margret@gmail.com>
2019-11-04 12:57:59 -0800

    Convert component to a class
    > Add elements' test ID
    > Add onclick event listener for submit button

--- gatsby/src/components/form.js
+++ gatsby/src/components/form.js
@@ -1,31 +1,50 @@
-import React from "react"
+import React from "react";
 
-import styles from "../css/form.module.css"
+import styles from "../css/form.module.css";
 
-const Form = () => (
-  <form className={styles.form}>
-    <label id="username" className="text-gray-700">
-      Username
-    </label>
-    <input
-      aria-labelledby="username"
-      type="text"
-      className="shadow border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-inner"
-    />
-    <label id="password" className="text-gray-700">
-      Password
-    </label>
-    <input
-      aria-labelledby="password"
-      type="password"
-      className="shadow border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-inner"
-    />
-    <button
-      className={`bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${styles.button}`}
-    >
-      Start snipping my recipes!
-    </button>
-  </form>
-)
+class Form extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         startLoginProcess: false
+      };
+   }
 
-export default Form
+   loginToPepperplate = e => {
+      e.preventDefault();
+   };
+
+   render() {
+      return (
+         <form className={styles.form}>
+            <label id="username" className="text-gray-700">
+               Username
+            </label>
+            <input
+               data-cy="usernameField"
+               aria-labelledby="username"
+               type="text"
+               className="shadow border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-inner"
+            />
+            <label id="password" className="text-gray-700">
+               Password
+            </label>
+            <input
+               data-cy="passwordField"
+               aria-labelledby="password"
+               type="password"
+               className="shadow border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-inner"
+            />
+            <button
+               data-cy="submitBtn"
+               className={`bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${styles.button}`}
+               onClick={this.loginToPepperplate}
+            >
+               Start snipping my recipes!
+            </button>
+         </form>
+      );
+   }
+}
+
+export default Form;
