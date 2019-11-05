a188b630dc8471e2faea66040ffd856149c96f78
Pax <pax.margret@gmail.com>
2019-11-05 14:54:29 -0800

    Fix test
    See commit c762bd8

--- api/tests/peppcrawler/test_login.py
+++ api/tests/peppcrawler/test_login.py
@@ -7,21 +7,22 @@ from peppcrawler import PepperplateCrawler
 
 @pytest.fixture()
 def crawler():
-  crawler = PepperplateCrawler()
-  return crawler
+    crawler = PepperplateCrawler()
+    return crawler
+
 
 def test_canVisitLoginPage(crawler):
-  response = crawler.visitLoginPage()
-  crawler.quitDriver()
-  assert response['title'] == 'Pepperplate'
-  
+    response = crawler.visitLoginPage()
+    crawler.quitDriver()
+    assert response['title'] == 'Pepperplate'
+
+
 def test_canLoginToPepperplate(crawler):
-  load_dotenv()
-  email = getenv("PEPPERPLATE_EMAIL")
-  password = getenv("PEPPERPLATE_PW")
-  crawler.visitLoginPage()
-  recipe_total = crawler.loginToPepperplate(email, password)
-  crawler.quitDriver()
-  
-  assert recipe_total == '315 RECIPES'
-  
+    load_dotenv()
+    email = getenv("PEPPERPLATE_EMAIL")
+    password = getenv("PEPPERPLATE_PW")
+    crawler.visitLoginPage()
+    success = crawler.loginToPepperplate(email, password)
+    crawler.quitDriver()
+
+    assert True == success
