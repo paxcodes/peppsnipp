9df19668f5f5a75677483f083b1cbcd2c7a059b6
Pax <pax.margret@gmail.com>
2019-11-21 15:01:10 -0800

    :bug: Fix oauth redirect uri

--- api/config.py
+++ api/config.py
@@ -1,18 +1,19 @@
 from os import getenv
 import redis
 
-class Config:
-  FLASK_APP = getenv('FLASK_APP')
-  SECRET_KEY = getenv('SECRET_KEY')
-  URL = getenv('URL')
-  
-  FLASK_ENV = getenv('FLASK_ENV')
-  TESTING = getenv('TESTING', False)
-  DEBUG = getenv('DEBUG')
 
-  SESSION_TYPE = getenv('SESSION_TYPE')
+class Config:
+    FLASK_APP = getenv('FLASK_APP')
+    SECRET_KEY = getenv('SECRET_KEY')
+    URL = getenv('URL')
+    CLIENT_URL = getenv('CLIENT_URL')
+
+    FLASK_ENV = getenv('FLASK_ENV')
+    TESTING = getenv('TESTING', False)
+    DEBUG = getenv('DEBUG')
+
+    SESSION_TYPE = getenv('SESSION_TYPE')
   SESSION_REDIS = redis.from_url(getenv('REDIS_URL'))
-  
-  DROPBOX_APP_KEY = getenv('DROPBOX_APP_KEY')
-  DROPBOX_APP_SECRET = getenv('DROPBOX_APP_SECRET')
-  
+
+    DROPBOX_APP_KEY = getenv('DROPBOX_APP_KEY')
+    DROPBOX_APP_SECRET = getenv('DROPBOX_APP_SECRET')
