c980e2440796498ebbc2dd34a04a03f4c4310093
Pax <pax.margret@gmail.com>
2019-11-22 21:05:24 -0800

    :sparkles: Add npm script to run api server automatically when testing
    Let's add a route that's needed by the npm script.

--- api/application/main.py
+++ api/application/main.py
@@ -3,8 +3,14 @@ from application.api_error import APIError
 
 main_blueprint = Blueprint('main_blueprint', __name__)
 
+
 @main_blueprint.app_errorhandler(APIError)
 def handle_apierror(error):
-  response = jsonify(error.to_dict())
-  response.status_code = error.status_code
-  return response
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@main_blueprint.route('/', methods=['HEAD'])
+def helloWorld():
+    return "Hello!"
