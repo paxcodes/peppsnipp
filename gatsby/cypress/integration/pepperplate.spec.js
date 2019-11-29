b87467f7e61c2b00f48ac1098ebb06d8904ac68b
Pax <pax.margret@gmail.com>
2019-11-28 17:37:35 -0800

    :white_check_mark: Write specs for logging in to Pepperplate

--- gatsby/cypress/integration/pepperplate.spec.js
+++ gatsby/cypress/integration/pepperplate.spec.js
@@ -7,10 +7,26 @@ describe("The Pepperplate Step", () => {
       "asks for Pepperplate's login credentials",
       tests.shouldAskForLoginCredentials
    );
-   it(
-      "displays a `loading gif` when submitting the form",
-      tests.shouldDisplayLoadingGif
-   );
+
+   context("when submitting the form", () => {
+      it(
+         "displays a progress bar with initial status",
+         tests.shouldDisplayProgressBarWithInitialStatus
+      );
+      // Submit button is not visible
+      // Progress bar is visible
+      // Progress bar value is 0
+      // Label says "Initiating login process.."
+
+      it("updates the progress bar when the browser starts");
+      // Value is updated to "1"
+      // Label is updated to "Login Progress Initiated..."
+
+      it("updates the progress bar when login page is loaded");
+      // Value is updates to "2"
+      // Label is updated...
+      // ? Assert whether the label has changed
+   });
 
    context("When api request to login to Pepperplate has completed", () => {
       it(
@@ -31,9 +47,13 @@ describe("The Pepperplate Step", () => {
 });
 
 context("When login is successful", () => {
-   it.only(
-      "notifies user when login is successful",
-      tests.shouldNotifyUserThatLoginIsSuccessful
-   );
-   it("removes the login form and describes the next process");
+   specify("the progress bar should be updated");
+   // Value is updated to "3"
+   // Label is updated to "Logged In!"
+
+   specify("the progress bar should be removed after a delay");
+
+   specify("the recipe count should be displayed");
+
+   specify("the next step should be focused");
 });
