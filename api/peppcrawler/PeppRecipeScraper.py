fd36a5b25bf0de12f7ee614c9505c2962dc51c6c
Pax <pax.margret@gmail.com>
2020-02-07 15:45:28 -0800

    :construction: Add class dedicated to scraping recipe page

---
+++ api/peppcrawler/PeppRecipeScraper.py
@@ -0,0 +1,37 @@
+class PeppRecipeScraper:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def Title(self):
+        pass
+
+    def Source(self):
+        pass
+
+    def Description(self):
+        pass
+
+    def Yield(self):
+        pass
+
+    def ActiveTime(self):
+        pass
+
+    def TotalTime(self):
+        pass
+
+    def Categories(self):
+        pass
+
+    def Ingredients(self):
+        pass
+
+    def Instructions(self):
+        pass
+
+    def Notes(self):
+        pass
+
+    def Image(self):
+        pass
